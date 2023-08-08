1710e6988216166ee94106e6eb22fd2c10bbf2b1
Uwe Köcher <mail@uwe.koecher.cc>
2023-08-09 01:11:26 +0200

    bloody hack to resolve level issue in the interval of [-1,3) including map update lsbg

--- lib/map/extractor/level_extractor.dart
+++ lib/map/extractor/level_extractor.dart
@@ -4,8 +4,12 @@ import 'package:mapsforge_flutter/maps.dart';
 
 class LevelExtractor {
   static Future<Map<int, String>> extractLevels(MapDataStore map) async {
-    var bbox = [map.boundingBox!.minLongitude, map.boundingBox!.maxLongitude,
-      map.boundingBox!.minLatitude, map.boundingBox!.maxLatitude];
+    var bbox = [
+      map.boundingBox!.minLongitude,
+      map.boundingBox!.maxLongitude,
+      map.boundingBox!.minLatitude,
+      map.boundingBox!.maxLatitude
+    ];
 
     var projection = MercatorProjection.fromZoomlevel(18);
     var tileX1 = projection.longitudeToTileX(bbox[0]);
@@ -16,18 +20,23 @@ class LevelExtractor {
     var tile1 = Tile(tileX1, tileY1, 18, 0);
     var tile2 = Tile(tileX2, tileY2, 18, 0);
 
-    var levels = await map.readPoiData(tile1, tile2)
-        .then((value) => value!.pointOfInterests
+    var levels = await map.readPoiData(tile1, tile2).then((value) => value!
+        .pointOfInterests
         .map((e) => e.tags.where((e) => e.key == 'level').map((t) => t.value!))
         .expand((element) => element)
         .map((element) => element.split(";"))
         .expand((element) => element)
-        .toSet().toList());
+        .toSet()
+        .toList());
 
-    levels.sort((a,b) => a.compareTo(b));
+    levels.sort((a, b) => a.compareTo(b));
     Map<int, String> levelsMap = {};
-    for (var i = 0; i < levels.length; ++i) {
-      levelsMap.putIfAbsent(i, () => levels[i]);
+    //for (var i = 0; i < levels.length; ++i) {
+    //  levelsMap.putIfAbsent(i, () => levels[i]);
+    //}
+    int i = -1;
+    for (final item in levels) {
+      levelsMap.putIfAbsent(i++, () => item);
     }
 
     return levelsMap;
